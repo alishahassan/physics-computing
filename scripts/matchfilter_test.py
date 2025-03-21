1517241d741332bd37ba3381f679fa949d0bea36
alhassan <alhassan@syr.edu>
2025-03-21 07:48:30 -0400

    updated file

--- scripts/matchfilter_test.py
+++ scripts/matchfilter_test.py
@@ -2,11 +2,11 @@ import numpy as np
 import mlx.core as mx
 from mlx.core.fft import ifft
 import time
-import concurrent.futures # python module that allows you to run functions in parallel using multiple threads
+import multiprocessing
 import matplotlib.pyplot as plt
 
 def define_match_filter(a, b, stream): # multiplies two arrays, performs the IFFT, and returns the maximum value of the reshaped result.
-    product = mx.multiply(a, b, stream=stream)
+    product = mx.multiply(a.conj(), b, stream=stream)
     ifft_result = ifft(product, stream=stream)
     reshaped_result = ifft_result.reshape(ifft_result.size) # reshapes the IFFT result
     return mx.eval(mx.max(reshaped_result, stream=stream)) # returns max val
@@ -18,12 +18,11 @@ def frozen_gpumatchfilter(ab):
                 return define_match_filter(*ab, stream=mx.gpu)
 
 if __name__ == "__main__":
-    list_size = 5
-    array_size = 1024
-
-    a_list = [mx.random.normal(shape=(array_size,), dtype=mx.float32) for _ in range(list_size)]
-    b_list = [mx.random.normal(shape=(array_size,), dtype=mx.float32) for _ in range(list_size)]
-
+    list_size = 120
+    array_size = int(2**20)
+    a_list = [mx.array(mx.random.normal(shape=(array_size,), dtype=mx.float32) + 1j * mx.random.normal(shape=(array_size,), dtype=mx.float32)) for _ in range(list_size)]
+    b_list = [mx.array(mx.random.normal(shape=(array_size,), dtype=mx.float32) + 1j * mx.random.normal(shape=(array_size,), dtype=mx.float32)) for _ in range(list_size)]
+    ab_list = list(zip(a_list,b_list))
     cpu_max_values = []
     gpu_max_values = []
 
@@ -36,17 +35,17 @@ if __name__ == "__main__":
     cpu_times_parallel = []
     gpu_times_parallel = []
 
-    test_cores = [1,2,3,4]
+    test_cores = [1,2,3]
 
     for ncores in test_cores:
-        with concurrent.futures.ProcessPoolExecutor(max_workers= ncores) as executor:
+        with multiprocessing.Pool(ncores) as pool:
             start_time = time.perf_counter()
-            cpu_results = list(executor.map(frozen_cpumatchfilter, zip(a_list, b_list)))
+            cpu_results = list(pool.map(frozen_cpumatchfilter, ab_list))
             cpu_time_parallel = time.perf_counter() - start_time
             cpu_times_parallel.append(cpu_time_parallel)
 
             start_time = time.perf_counter()
-            gpu_results = list(executor.map(frozen_gpumatchfilter, zip(a_list, b_list)))
+            gpu_results = list(pool.map(frozen_gpumatchfilter, ab_list))
             gpu_time_parallel = time.perf_counter() - start_time
             gpu_times_parallel.append(gpu_time_parallel)
 
@@ -59,4 +58,7 @@ if __name__ == "__main__":
     plt.title("Performance of Match Filter on CPU vs GPU (Parallelized)")
     plt.legend()
     plt.grid(True)
+
+    # saving plot
+    plt.savefig("match_filter_performance.png")
     plt.show()
